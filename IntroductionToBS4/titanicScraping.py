9ad9c030e717956912b68ab63bc38dc8136dd44f
armanjscript <armandaneshdoost@gmail.com>
2024-03-16 00:14:19 +0330

    titanicScraping.py added

---
+++ IntroductionToBS4/titanicScraping.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup 
+
+website = 'https://subslikescript.com/movie/Titanic-120338'
+result = requests.get(website)
+content = result.text
+
+soup = BeautifulSoup(content, 'lxml')
+#print(soup.prettify())
+
+box = soup.find('article', class_='main-article')
+title = box.find('h1').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
+
+
+print(transcript)
+print(f'title is: {title}')
