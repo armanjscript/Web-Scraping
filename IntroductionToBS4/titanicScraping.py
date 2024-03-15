f31e2e008709e377f472378527df9522a23da24e
armanjscript <armandaneshdoost@gmail.com>
2024-03-16 02:04:39 +0330

    Titanic Scraping with BS4 approved

--- IntroductionToBS4/titanicScraping.py
+++ IntroductionToBS4/titanicScraping.py
@@ -13,5 +13,9 @@ title = box.find('h1').get_text()
 transcript = box.find('div', class_='full-script').get_text(strip=True, separator=' ')
 
 
-print(transcript)
-print(f'title is: {title}')
+# print(transcript)
+# print(f'title is: {title}')
+
+
+with open(f'{title}.txt', 'w', encoding='utf8') as file:
+    file.write(transcript)
